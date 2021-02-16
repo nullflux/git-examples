c472255c3363771b66be9690583792c9e5575e71
Adam Weber <a.diamond.weber@gmail.com>
2021-02-15 22:25:48 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2), {
+  alias: {
+    h: ["help", "?"],
+    p: ["proceed"],
+  },
+});
+
+function printUsage() {
+  ["Usage: node index.js [-p, --proceed] [-h, --help]", ""].forEach((x) =>
+    console.log(x)
+  );
+  process.exit();
+}
+
+async function main() {
+  if (args.help) {
+    printUsage();
+  }
+
+  if (!args.proceed) {
+    console.log("\n--proceed is required.\n");
+    printUsage();
+  }
+
+  console.log("Hello, world!");
+}
+
+main().catch((e) => {
+  console.error("Unhandled exception", e);
+});
