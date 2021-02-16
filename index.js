d7718afefa25cdb270faff7eb21ca20e0404706d
Adam Weber <a.diamond.weber@gmail.com>
2021-02-15 22:57:02 -0800

    Add slow-writer

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ function printUsage() {
   process.exit();
 }
 
+function sleep(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, 1000 * seconds));
+}
+
+async function writeSlow(str) {
+  const values = str.split("");
+  for (char of values) {
+    process.stdout.write(char);
+    await sleep(0.1 * Math.random());
+  }
+  process.stdout.write("\n");
+}
+
 async function main() {
   if (args.help) {
     printUsage();
@@ -26,7 +39,11 @@ async function main() {
     printUsage();
   }
 
-  console.log("Hello, world!");
+  await writeSlow(`Hello, world
+This is a longer corpus of text, which will take some time to put on the screen
+
+Tbh stumptown unicorn biodiesel actually. Tofu four loko squid flannel wolf PBR&B activated charcoal kogi deep v single-origin coffee hoodie unicorn. You probably haven't heard of them hella single-origin coffee put a bird on it before they sold out. Farm-to-table green juice offal chia poke pop-up freegan locavore franzen craft beer af synth. Flannel banh mi snackwave, squid fixie shoreditch church-key.
+`);
 }
 
 main().catch((e) => {
